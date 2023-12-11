324b1942e7c4a17da53b56803732885ab7b4e0ff
Sean Parker <seanrichardparker@gmail.com>
2023-12-11 15:35:14 -0500

    boat_race logic

--- d6/boat_race.rb
+++ d6/boat_race.rb
@@ -3,27 +3,42 @@ races = <<RACES
   Distance:  9  40  200
 RACES
 
+# races = <<RACES
+#   Time:        59     68     82     74
+#   Distance:   543   1020   1664   1022
+# RACES
 
-time = races.split("\n")[0]
-distance = races.split("\n")[1]
+# breaking down the input
+time_row = races.split("\n")[0]
+distance_row = races.split("\n")[1]
 
-puts "time #{time.split(":")[1].split(" ")}"
-puts "distance #{distance.split(":")[1].split(" ")}"
-# first = RACES.split("\n")
-# puts races.split("\n")[0].split(":")[1].split(" ")[0]
+time_arr = time_row.split(":")[1].split(" ").map!(&:to_i)
+distance_arr = distance_row.split(":")[1].split(" ").map!(&:to_i)
+
+puts "time_arr: #{ time_arr }"
+puts "distance_arr: #{distance_arr}"
+
+# calculate possible outcomes for each race
+time_arr.each do |num|
+  race_arr = *(1..num)
+  
+  puts race_arr
+end
+
+
+# first_race = {
+#   0 => 0, 
+#   1 => 6, 
+#   2 => 10, 
+#   3 => 12, 
+#   4 => 12, 
+#   5 => 10, 
+#   6 => 6, 
+#   7 => 0
+# }
 
 
 
-first_race = {
-  0 => 0, 
-  1 => 6, 
-  2 => 10, 
-  3 => 12, 
-  4 => 12, 
-  5 => 10, 
-  6 => 6, 
-  7 => 0
-}
 
 
 
